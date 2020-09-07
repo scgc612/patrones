5a3e4059b7a9b2ef85078b29d317fe37ab7afb05
Santiago García <scgc612@gmail.com>
2020-09-06 23:38:32 -0300

    Solucion para expert y SRP

---
+++ Ejercicio 3/src/Library/Doctor.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Text;
+
+namespace Library
+{
+    //Aplicando SRP crearía dos clases más, una con los datos de los Doctores y otra con los datos
+    //de los Pacientes, y aplicando EXPERT haría que AppointmentService le pida la informacion a los 
+    //expertos de la informacion Doctores y Pacientes
+    public class Doctor
+    {
+        public static bool IsValid(string algo)
+        {
+            if (string.IsNullOrEmpty(algo))
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+       
+    }    
+
+}
